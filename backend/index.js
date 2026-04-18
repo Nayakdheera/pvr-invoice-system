9ff0dcf621de36f227f7b92ff02ba24cfe5fcb8c
Nayakdheera <nayakdheera@gmail.com>
2026-04-18 11:15:19 +0530

    fix: cors issue

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
